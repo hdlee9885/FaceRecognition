aa0fc02a00652770816e53144d75ac2f9db12d1a
Haosong Li <hdlee9885@hotmail.com>
2021-01-12 22:55:44 -0600

    Able to detect in webcam

--- face_rec.py
+++ face_rec.py
@@ -6,24 +6,31 @@ from random import randrange
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Choose an image to detect face in
-img = cv2.imread('RDJ.jpg')
+# img = cv2.imread('RDJ.jpg')
+webcam = cv2.VideoCapture(0)
+
+while True:
+    
+    # read the current frame
+    frame_read, frame = webcam.read()
+
+    # Must convert to greyscale
+    greyscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    face_coordinates = trained_face_data.detectMultiScale(greyscaled_img)
+
+    for (x, y, w, h) in face_coordinates:
+        # Draw rectangles around faces
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 2)
 
 
-# Must convert to greyscale
-greyscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Me', frame)
+    key = cv2.waitKey(1)   
 
-# Detect faces
-face_coordinates = trained_face_data.detectMultiScale(greyscaled_img)
+    if key == 81 or key == 113:
+        break
 
-for (x, y, w, h) in face_coordinates:
-    # Draw rectangles around faces
-    cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 2)
-
-
-# print(face_coordinates)
-
-cv2.imshow('Me', img)
-cv2.waitKey()
-
+webcam.release()
 
 print("code completed")
